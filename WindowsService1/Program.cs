21139df25fd1cb8eb820cf6a82e27deb2160f8df
rundeploy <cristianmitul@gmail.com>
2018-12-07 11:57:30 +0000

    Added some final commenting for the documentation and some generic code for more folder options and another substring for filenames

--- WindowsService1/Program.cs
+++ WindowsService1/Program.cs
@@ -10,37 +10,37 @@ namespace WindowsService1
         private const string EDP = "edp";
         private const string BCP = "bcp";
 
-        //[TODO] ask user the FROM folder and the TO folder files to move
         private static string path1 = @"D:\Downloads\Pasta1"; // Specify folder from you want to move files
         private static string path2 = @"D:\Downloads\Pasta2"; // Specify folder where you want the files be moved to
         private static string path3 = @"D:\Downloads\Pasta3"; // Specify folder where you want the files be moved to
+        //private static string path4 = @"D:\Downloads\Pasta4"; // Specify folder where you want the files be moved to
 
         /// <summary>
-        /// Just move pdf files from folder in path1 to folder in path2 timing 30s to 30s
+        /// Just move pdf files from folder in path1 to destination folder
         /// @author rundeploy
         /// @date 4/12/2018
         /// </summary>
         static void Main()
         {
 
-            //Timer aTimer = new Timer();
-            //aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-            //aTimer.Interval = 5000;
-            //aTimer.Enabled = true;
-            metodo();
+            Timer aTimer = new Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000; // 5000 = From 5 seconds to 5 seconds 
+            aTimer.Enabled = true;
+            
 
-            //Console.WriteLine("Press \'q\' to quit the sample."); 
-            //while (Console.Read() != 'q') ; // Mod this in order to run forever
+            Console.WriteLine("Press \'q\' to quit the sample."); 
+            while (Console.Read() != 'q') ; // Mod this in order to run forever
         }
 
         /// <summary>
-        /// Actual method that moves the files
+        /// While there are pdf files in the path1 folder check if on destination exist same name file
+        /// if exist then modify name until there does not exist and then move, 
+        /// otherwise just move the file
         /// </summary>
         /// <param name="source"></param>
         /// <param name="a"></param>
-        //private static void OnTimedEvent(object source, ElapsedEventArgs a)
-
-        private static void metodo()
+        private static void OnTimedEvent(object source, ElapsedEventArgs a)
         {
             //Guarantee that folders do exist
             if (!Directory.Exists(path1))
@@ -57,17 +57,13 @@ namespace WindowsService1
                         Directory.CreateDirectory(path2);
                     if (!Directory.Exists(path3))
                         Directory.CreateDirectory(path3);
+                    //if (!Directory.Exists(path4))
+                    //    Directory.CreateDirectory(path4);
 
-
-                    //if filename contains edp
-                    //move to folder 2
-                    //else if filename contains bcp
-                    // move to folder3
-
-                    if(IsSubstring(name, EDP)){
+                    if (IsSubstring(name, EDP)) {
                         if (File.Exists(Path.Combine(path2, name)))
                         {
-                            string newName = GetNotDuplicatedFileName(path2, name); //Alter the duplicated name then move it
+                            string newName = GetNotDuplicatedFileName(path2, name); //file with same name exist so lets Alter the duplicated name 
                             File.Move(Path.Combine(path1, name), Path.Combine(path2, newName));
                             Console.WriteLine("{0} was moved to {1}.", path1, path2);
                         }
@@ -76,11 +72,11 @@ namespace WindowsService1
                             File.Move(Path.Combine(path1, name), Path.Combine(path2, name));
                             Console.WriteLine("{0} was moved to {1}.", path1, path2);
                         }
-                    }else if (IsSubstring(name, BCP))
+                    } else if (IsSubstring(name, BCP))
                     {
                         if (File.Exists(Path.Combine(path3, name)))
                         {
-                            string newName = GetNotDuplicatedFileName(path3, name); //Alter the duplicated name then move it
+                            string newName = GetNotDuplicatedFileName(path3, name); //file with same name exist so lets Alter the duplicated name 
                             File.Move(Path.Combine(path1, name), Path.Combine(path3, newName));
                             Console.WriteLine("{0} was moved to {1}.", path1, path3);
                         }
@@ -89,46 +85,41 @@ namespace WindowsService1
                             File.Move(Path.Combine(path1, name), Path.Combine(path3, name));
                             Console.WriteLine("{0} was moved to {1}.", path1, path3);
                         }
+                    //}else if (IsSubstring(name, QWERT))
+                    //{
+                    //    if (File.Exists(Path.Combine(path4, name)))
+                    //    {
+                    //        string newName = GetNotDuplicatedFileName(path4, name); //file with same name exist so lets Alter the duplicated name 
+                    //        File.Move(Path.Combine(path1, name), Path.Combine(path4, newName));
+                    //        Console.WriteLine("{0} was moved to {1}.", path1, path3);
+                    //    }
+                    //    else
+                    //    {
+                    //        File.Move(Path.Combine(path1, name), Path.Combine(path4, name));
+                    //        Console.WriteLine("{0} was moved to {1}.", path1, path4);
+                    //    }
 
                     }else
                     {
-                        Console.WriteLine("Nothing to move");
+                        Console.WriteLine("No files to move");
                     }
-
-                    
-                    
-                    
-
-
-                    // See if the original exists now.
-                    if (File.Exists(path1 + pdfFileNames))
-                    {
-                        Console.WriteLine("The original file still exists, which is unexpected.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("The original file no longer exists, which is expected.");
-                    }
-
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("The process failed: {0}", e.ToString());
+                Console.WriteLine("The process of moving files failed: {0}", e.ToString());
             }
         }
        
 
         /// <summary>
-        /// Get not duplicated filename
+        /// Get different name than one that exist on destination folder in order to not to overwrite and loose it
         /// </summary>
         /// <param name="destPath">destination folder</param>
         /// <param name="name">file name</param>
-        /// <returns></returns>
+        /// <returns>Returns a new name guaranteeing that it doesnt exist in destination folder</returns>
         private static string GetNotDuplicatedFileName(string destPath, string name)
         {
-            //bool alterNameSuccessed = false;
-
             string alteredName = actuallyAlterDupName(name);
 
             if (File.Exists(Path.Combine(destPath, alteredName)))
@@ -140,10 +131,10 @@ namespace WindowsService1
         }
 
         /// <summary>
-        /// Alter the names of files if they are the same on destination folder as in the origin
+        /// Alter the names of files if they are the same on destination folder as in the origin and returns a string of the altered name
         /// </summary>
         /// <param name="name">file name to be changed if duplicated on the destination folder</param>
-        /// <returns></returns>
+        /// <returns>Get the new name of the file</returns>
         private static string actuallyAlterDupName(String name)
         {
             string alteredName = "";
@@ -226,6 +217,12 @@ namespace WindowsService1
             }
         }
 
+        /// <summary>
+        /// Verify if in the name exist a substring substr
+        /// </summary>
+        /// <param name="name">String to verify if substr is in</param>
+        /// <param name="substr">Substring to verify if it is in name string</param>
+        /// <returns>Return true if substring substr is a part of the string name</returns>
         private static bool IsSubstring(string name, string substr)
         {
             if (name.ToLower().Contains(substr))
